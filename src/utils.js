af37263212678d849cde7d5360835cd7be7ef195
Ceci007 <cecibenitezca@gmail.com>
2021-05-10 17:33:50 -0400

    Configuring webpack

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+console.log('utils is running!');
+
+const square = x => x * x;
+
+export { square };
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// const mode = "development"; 
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  }
+};
