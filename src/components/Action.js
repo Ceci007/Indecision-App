77d6541c753e89d4ee42debc199305396ed9d33a
Ceci007 <cecibenitezca@gmail.com>
2021-05-14 13:20:30 -0400

    disabling button conditionally

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,16 +3,17 @@ import userPhoto from '../img/user.jpg';
 
 const Action = (props) => (
   <div className="user-header">
-    <div      
-      onClick={props.handlePick} 
-      disabled={!props.hasOptions} >
+    <div>
       <div className="user">
         <div className="user__photo">
           <img className="user__photo--img" src={userPhoto} alt="Yuki Lee" />
         </div>
         <span className="user__name">Yuki Lee</span>  
       </div>
-      <button className="big-button">
+      <button 
+        className="big-button"
+        onClick={props.handlePick} 
+        disabled={!props.hasOptions} >
         What should I do?
       </button>
     </div>
